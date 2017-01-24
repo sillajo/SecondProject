9383c837d55c263f6c55023d3c91d4149d4e8bc9
sillajo <23rn@Naver.com>
2017-01-24 14:47:04 +0900

    GitHub for git pull

--- Hello.java
+++ Hello.java
@@ -5,11 +5,13 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
+	        System.out.println("what is Git?"
 		System.out.println("thats dvcs");
+	        System.out.println("That's version Control);
 		
 	}       
 
-                System.out.println("what is Git?"
+               
 	
 	}
 
